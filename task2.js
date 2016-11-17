874d92b6d964b5ec5b0546ad4dcc9c9ae4bf2638
benjihannam <benjamin.c.hannam.18@dartmouth.edu>
2016-11-17 17:06:50 -0500

    more stuff

--- task2.js
+++ task2.js
@@ -159,11 +159,42 @@ var ValueVertexSource = `
 var ValueFragmentSource = `
     precision mediump float;
 
+    const vec3 LightPosition = vec3(4.0, -4.0, 10.0);
+    const vec3 LightIntensity = vec3(400.0);
+    uniform mat4 ViewInverse;
+    uniform mat4 ModelInverse;
+    const float n = 60.0;
+
+    varying vec3 vNormal;
+    varying vec4 globalPosition;
 
     varying vec2 vTextureCoord;
     uniform sampler2D uSampler;
+    uniform float time;
 
     void main() {
+
+        //get the light vector and the eye vector
+        vec3 l = LightPosition - vec3(globalPosition);
+        vec3 view_direction = vec3(ViewInverse * vec4(0.0, 0.0, 0.0,1.0))  - vec3(globalPosition);
+
+
+        // combine them and normalize to get h
+        vec3 combined = normalize(l) + normalize(view_direction);
+        vec3 h = normalize(combined);
+
+        //get the normal vector
+        vec3 n2 = vec3(ModelInverse * vec4(vNormal, 1.0));
+
+        // calculate the dot product 
+        float n_dot_h = dot(normalize(n2), normalize(h));
+
+        //get the amount of incident light falling on the point
+        vec3 incident_light = LightIntensity / pow(length(l), 2.0);
+
+
+        vec3 ks = vec3(0.0);
+
         float value = 0.0;
         float size = 64.0;
         float init_size = 64.0;
@@ -171,26 +202,32 @@ var ValueFragmentSource = `
             value += (texture2D(uSampler, vTextureCoord/size))[0] * size;
             size = size / 2.0;
         }
-        value = value / 128.0;
+        value = value / 128.0 + 10.0 * time;
+
         vec4 color = vec4(value, value, value, 1.0);
         if(value < 0.2){
             color = vec4(0.0, 0.0, 0.5, 1.0);
+            ks = vec3(0.1);
         }
         else if(value < 0.4){
            color = vec4(0.0, 0.0, 1.0, 1.0);
+           ks = vec3(0.1);
        }
        else if(value < 0.42){
            color = vec4(0.0, 1.0, 1.0, 1.0);
+           ks = vec3(0.1);
        }
        else if(value < 0.8){
            color = vec4(0.0, 0.5, 0.0, 1.0);;
+           
        }
        else{
            color = vec4(0.5, 0.4, 0.1, 1.0);;
        }
 
-        gl_FragColor = color;
-        //gl_FragColor = texture2D(uSampler, vTextureCoord);
+         //calculate the light components
+        vec3 spec_light = ks * incident_light * pow(n_dot_h, n);
+        gl_FragColor = color + vec4(spec_light, 0.0);
     }
 `;
 
@@ -255,6 +292,11 @@ ShadedTriangleMesh.prototype.render = function(gl, model, view, projection) {
 
     var modelInverseMatrix = gl.getUniformLocation(this.shaderProgram, "ModelInverse");
     gl.uniformMatrix4fv(modelInverseMatrix, false, model.inverse().m);
+
+    
+    var time = gl.getUniformLocation(this.shaderProgram, "time");
+    gl.uniform1f(time, Date.now());
+    //console.log(Math.sin(Date.now()));
 
 
     // var texcoordLocation = gl.getAttribLocation(program, "a_texcoords");
@@ -446,7 +488,7 @@ Task2.prototype.render = function(gl, w, h) {
     var rotation = Matrix.rotate(Date.now()/25, 0.4327, 1, 0);
     var cubeModel = Matrix.translate(-3.8, 2, 0).multiply(rotation).multiply(Matrix.scale(0.3, 0.3, 0.3));
     var sphereModel = Matrix.translate(-1.8, 0, 0).multiply(rotation).multiply(Matrix.scale(1.2, 1.2, 1.2));
-    var sphereModel2 = Matrix.translate(1.8, 0, 0).multiply(rotation).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    var sphereModel2 = Matrix.translate(1.8, 0, 0).multiply(rotation).multiply(Matrix.scale(1.1, 1.1, 1.1));
 
     //this.sphereMesh.render(gl, sphereModel2, view, projection);
     this.earthMesh.render(gl, sphereModel, view, projection, this.earthTexture);
